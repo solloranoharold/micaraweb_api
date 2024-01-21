12ad99d93eacc2ef07d8d1e01f0228d90743bd60
haroldsollorano <haroldsollorano@awesomelabph.com>
2024-01-21 13:33:39 +0800

    revisions_2

--- routes/monitoring.js
+++ routes/monitoring.js
@@ -37,15 +37,16 @@ router.post("/insertUpdateRequest", (req, res) => {
   let sql = "";
   if (req.body.index == -1) {
     sql = `INSERT INTO tbl_transactions
-    (user_id , visitor_name, vehicle , plate_no ,purpose)
+    (user_id , visitor_name, vehicle , plate_no ,purpose , date_arrival )
     values
-    (${req.body.user_id},'${req.body.visitor_name}','${req.body.vehicle}','${req.body.plate_no}','${req.body.purpose}')`;
+    (${req.body.user_id},'${req.body.visitor_name}','${req.body.vehicle}','${req.body.plate_no}','${req.body.purpose}','${req.body.date_arrival}')
+    `;
   } else {
     sql = `UPDATE tbl_transactions SET user_id=${req.body.user_id} , visitor_name = '${req.body.visitor_name}'
     , vehicle='${req.body.vehicle}' , plate_no='${req.body.plate_no}' ,purpose='${req.body.purpose}'
     ,date_arrival='${req.body.date_arrival}',date_departure='${req.body.date_departure}',checkedBy='${req.body.checkedBy}' WHERE transaction_id=${req.body.transaction_id}`;
   }
-  console.log("post tansacation", sql);
+
   dbCon.query(sql, function (error, results, fields) {
     if (error) throw error;
     res.send(results);
