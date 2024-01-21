ea80702a6d1d041f91050472b397985d6477b4e8
haroldsollorano <haroldsollorano@awesomelabph.com>
2024-01-21 13:49:05 +0800

    revisions_5

--- routes/monitoring.js
+++ routes/monitoring.js
@@ -43,7 +43,11 @@ router.post("/insertUpdateRequest", (req, res) => {
   } else {
     sql = `UPDATE tbl_transactions SET user_id=${req.body.user_id} , visitor_name = '${req.body.visitor_name}'
     , vehicle='${req.body.vehicle}' , plate_no='${req.body.plate_no}' ,purpose='${req.body.purpose}'
-    ,date_arrival='${req.body.date_arrival}',date_departure='${req.body.date_departure}',checkedBy='${req.body.checkedBy}' WHERE transaction_id=${req.body.transaction_id}`;
+    ,date_arrival='${req.body.date_arrival}',checkedBy='${req.body.checkedBy}' `;
+    sql +=
+      req.body.date_departure ||
+      `',date_departure='${req.body.date_departure}'`;
+    sql += `WHERE transaction_id=${req.body.transaction_id}`;
   }
   console.log("post transacation", sql);
   dbCon.query(sql, function (error, results, fields) {
