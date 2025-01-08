f67daf02564c0c96af0a72bad64322e312234551
solloranoharold <solloranoharold123@gmail.com>
2025-01-08 18:11:13 +0800

    changes

--- routes/db.js
+++ routes/db.js
@@ -1,14 +1,14 @@
 var mysql = require("mysql");
-var connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "micaraweb",
-});
 // var connection = mysql.createConnection({
-//   host: "bkcx36iivyb7kacrvmoc-mysql.services.clever-cloud.com",
-//   user: "u9wqtgjkmr73exdf",
-//   password: "5lvYH0AbcUyWIXGrojvh",
-//   database: "bkcx36iivyb7kacrvmoc",
+//   host: "localhost",
+//   user: "root",
+//   password: "",
+//   database: "micaraweb",
 // });
+var connection = mysql.createConnection({
+  host: "bkcx36iivyb7kacrvmoc-mysql.services.clever-cloud.com",
+  user: "u9wqtgjkmr73exdf",
+  password: "5lvYH0AbcUyWIXGrojvh",
+  database: "bkcx36iivyb7kacrvmoc",
+});
 module.exports = connection;
